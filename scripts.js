ea6787b1e0c994b73ab2f341f89295bcac761ba6
Ashani Mae <flava4lyphe92@gmail.com>
2023-11-05 22:26:15 -0500

    updated project files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
 /* Add your JavaScript to this file */
+window.addEventListener('DOMContentLoaded', () => {
+    let form = document.querySelector(".form-group");
+    const message = document.querySelector(".message");
+    let email = document.getElementById("email");
+
+    const button = document.querySelector("button.btn");
+    function addMsg(email){
+        
+        if (email.value == ""){
+            message.innerHTML="Thank you! Please enter valid email address";
+            // message.classlist.add ("message");
+        } else {
+            message.innerHTML=`Thank you! Your email 
+            address ${email.value} has been added to our mailing list!`;
+        }
+        
+    }
+    button.addEventListener ("click",(e) => {
+        e.preventDefault()
+        addMsg(email); 
+    });
+
+})
